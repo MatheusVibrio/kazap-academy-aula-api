e78991e219948b26a84f3e595bf3a50d26d690c9
MatheusVibrio <matheusvibrio@hotmail.com>
2021-07-15 20:26:51 -0300

    Minha primeira aplicação na Aula de API com README

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const app = require('./src/app')
 const secrets = require('./secrets.json')
 
 app.listen(secrets.port, () => {
-    console.log(`Server is up and running on http://localhost:${secrets.port}`)
+    console.log(`Server is up and running on http://localhost:${secrets.port} - enjoy!`)
 })
